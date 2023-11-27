a42c9732e7ffbe8a72735b2affdb604431a66c90
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-11-27 15:24:06 -0700

    extracting dynamic parameter values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,15 @@
+import { useRouter} from "next/router"
+
 // our-domain.com/
 
 function DetailPage() {
+const router = useRouter()
+
+ const newsId =  router.query.newsId;
+
+ //SEND A REQUEST TO THE BACKEND API
+ // TO FETCH THE NEWS ITEM WITH NEWSId
+
     return <h1>THE DETAIL PAGE</h1>
     }
     
